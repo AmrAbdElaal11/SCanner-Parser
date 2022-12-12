0f887b0b9e1df845d427211681d9849e9d417d09
amr3b3al <amrmohamed362363@gmail.com>
2022-12-12 19:30:58 +0200

    final Scanner code

--- src/Scanner.java
+++ src/Scanner.java
@@ -19,17 +19,16 @@ public class Scanner {
     public static ArrayList<Token> Tokens = new ArrayList<>();
     public static File inputFile ;
     public static File ouputFile;
-   //takes array of token and write it to the file in the form of value type pairs
-    public static void writeTofile(ArrayList<Token> tokens,File file) throws IOException {
-       String string = "\n : (TOKEN_Value  ,   TYPE)";
-       Files.writeString(Paths.get(file.getAbsolutePath()),string , StandardOpenOption.CREATE,StandardOpenOption.TRUNCATE_EXISTING);
-       for (int k =0;k<tokens.size();k++){
-           Token token = Tokens.get(k);
-           Files.writeString(Paths.get(file.getAbsolutePath()),"\n"+k+": ("+token.stringVal+" ,  "+token.tokenType.name()+")", StandardOpenOption.APPEND);
-       }
 
+
+   //takes array of token and write it to the file in the form of value type pairs
+    public static void writeTofile(Token token,File file,int k) throws IOException {
+       String string = "\n : [TOKEN_Value  ,   TYPE]";
+       if(k==0)
+       Files.writeString(Paths.get(file.getAbsolutePath()),string , StandardOpenOption.CREATE,StandardOpenOption.TRUNCATE_EXISTING);
+       Files.writeString(Paths.get(file.getAbsolutePath()),"\n"+k+": ["+token.stringVal+" ,  "+token.tokenType.name()+"]", StandardOpenOption.APPEND);
     }
-public static void  scan(String tiny,Index index,ArrayList<Token> tokens){
+public static Token  scan(String tiny,Index index,ArrayList<Token> tokens){
 
     Token token =new Token();
     boolean parserToken =false;
@@ -39,8 +38,13 @@ public static void  scan(String tiny,Index index,ArrayList<Token> tokens){
 
     for (;index.i<=charsInput.length;(index.i)++){
         String curr="";
+
         if(index.i< charsInput.length) {
+
              curr = String.valueOf(charsInput[index.i]);
+        }
+        else{
+            return  null;
         }
 
         switch (current)
@@ -113,33 +117,26 @@ public static void  scan(String tiny,Index index,ArrayList<Token> tokens){
                 }
                 break;
             case DONE:
-
-                 index.i--;
+                 //index.i--;
                  token = new Token(tokenVal.toString());
                  tokens.add(token );
-
-                parserToken =true;
-                current=STATE.START;
-                tokenVal=new StringBuffer();
-
-                break;
+                 parserToken =true;
+                 current=STATE.START;
+                 tokenVal=new StringBuffer();
+                 break;
         }
-
-
-//        if (parserToken){
-//
-//            break;}
-
-
+       if (parserToken){
+            break;
+       }
     }
-//    return token;
+    return token;
 }
 
 
 
 
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -149,15 +146,15 @@ public static void  scan(String tiny,Index index,ArrayList<Token> tokens){
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
+                // file chooser to select file from any directory
                 JFileChooser jfc = new JFileChooser();
                 jfc.showOpenDialog(null);
-
                 inputFile=jfc.getSelectedFile();
-                System.out.println(inputFile.getParent());
+                // read the file content in one line
                 String code = ReadFile(inputFile);
                 System.out.println(code);
 
-                scan(code,i,Tokens);
+                // output file path relative to the input file
                 String path = inputFile.getParent()+"\\Tokens.txt";
                 ouputFile= new File(path);
                 try {
@@ -171,21 +168,27 @@ public static void  scan(String tiny,Index index,ArrayList<Token> tokens){
                     System.out.println("An error occurred.");
                     e.printStackTrace();
                 }
-                try {
-                    writeTofile(Tokens,ouputFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                // write tokens to ouput file
+                int k=0;
+                // scan the file for token extraction and write to output file
+                while(true){
+                    Token t = scan(code,i,Tokens);
+                    if(t==null) break;
+                    try {
+                        writeTofile(t,ouputFile,k);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    k++;
 
+                }
             }
         });
-
-        //File inputFile = new File("src/TINY.txt");
-
-
-
-
 }
+
+
+
+
 
     public static String ReadFile(File file){
         StringBuffer input =new StringBuffer();
@@ -193,8 +196,10 @@ public static void  scan(String tiny,Index index,ArrayList<Token> tokens){
         try{
             java.util.Scanner scan = new java.util.Scanner(file);
             input.append(scan.nextLine());
+
             while(scan.hasNextLine()){
                 input.append(scan.nextLine());
+                input.append(" ");
             }
         }
         catch(FileNotFoundException ex){
--- src/Token.java
+++ src/Token.java
@@ -2,35 +2,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 enum TokenType{
-    RESERVED,IDENTIFIER,NUMBER,SPECIALSYMBOL,ASSIGN
+    RESERVED,IDENTIFIER,NUMBER,SPECIALSYMBOL,ASSIGN,IF , THEN, ELSE , END , READ , WRITE , UNTIL,REPEAT,
+    SEMICOLON, LESSTHAN,GREATERTHAN, EQUAL, PLUS,MINUS , MULT, DIV ,OPENBRACKET,CLOSEDBRACKET
+
 }
+
+
+
+
 
 public class Token {
 
     public TokenType tokenType;
     public String stringVal;
     public int  numVal;
-    public static Map<String, String> reserved = new HashMap<String, String>() {{
-        put("if","if");
-
-        put("else","else");
-        put("repeat","repeat");
-        put("until","until");
-        put("end","end");
-        put("read","read");
-        put("write","write");
-        put("then","then");
+    Map<String,Enum<TokenType>> enumMap= new HashMap<String, Enum<TokenType>>();
+    ;
+    public static Map<String, Enum<TokenType>> reserved = new HashMap<String, Enum<TokenType>>() {{
+        put("if",TokenType.IF);
+        put("else",TokenType.ELSE);
+        put("repeat",TokenType.REPEAT);
+        put("until",TokenType.UNTIL);
+        put("end",TokenType.END);
+        put("read",TokenType.READ);
+        put("write",TokenType.WRITE);
+        put("then",TokenType.THEN);
     }};
-    public static Map<String, String> symbols = new HashMap<String, String>() {{
-        put("+","+");
-        put("-","-");
-        put("/","/");
-        put("=","=");
-        put("<","<");
-        put("*","*");
-        put("(","(");
-        put(")",")");
-        put(";",";");
+    public static Map<String, Enum<TokenType>> symbols = new HashMap<String, Enum<TokenType>>() {{
+        put("+",TokenType.PLUS);
+        put("-",TokenType.MINUS);
+        put("/",TokenType.DIV);
+        put("=",TokenType.EQUAL);
+        put(">",TokenType.GREATERTHAN);
+        put("<",TokenType.LESSTHAN);
+        put("*",TokenType.MULT);
+        put("(",TokenType.OPENBRACKET);
+        put(")",TokenType.CLOSEDBRACKET);
+        put(";",TokenType.SEMICOLON);
     }};
     public static String assign=":=";
     public Token(){
@@ -41,17 +49,18 @@ public class Token {
         this.stringVal=stringVal;
 
         if (reserved.containsKey(stringVal)){
-            System.out.println(stringVal);
-            this.tokenType=TokenType.RESERVED;
+           // System.out.println(stringVal);
+            this.tokenType= (TokenType) reserved.get(stringVal);
         }
         else if (symbols.containsKey(stringVal)){
-            this.tokenType=TokenType.SPECIALSYMBOL;
+            this.tokenType=(TokenType) symbols.get(stringVal);
         }
         else if (assign.equals(stringVal)){
             this.tokenType=TokenType.ASSIGN;
         }
         else if (stringVal.matches("[0-9]*")){
             this.tokenType=TokenType.NUMBER;
+            this.numVal=Integer.valueOf(stringVal);
         }
         else{
             this.tokenType=TokenType.IDENTIFIER;
